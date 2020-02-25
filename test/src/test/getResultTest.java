bdc7c860510ff29a409629293d624ef3c6817b3d
lihaobin <969882971@qq.com>
2020-02-25 17:26:51 +0800

    commit2

--- test/src/test/getResultTest.java
+++ test/src/test/getResultTest.java
@@ -17,7 +17,7 @@ public class getResultTest {
 	}
 	
 	public static void main(String[] args) {
-		//至此，通过一个工厂类，在我们在 getResult（）调用的时候，根据传入 type，即可获取到 对应 Strategy
+		//至此，通过一个工厂类，在我们在 getResult（）调用的时候，根据传入 type，即可获取到 对应 Strategy.
 		System.out.println(getResult(1000, 3));
 		
 	}
